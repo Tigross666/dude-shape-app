205b42f83f415fd80abec2c0ef6e228d647e5cb6
Artur Protsenko <aeprotsenko@avito.ru>
2026-03-18 22:09:27 +0300

    chore: remove legacy сomponents/ directory and old App files

--- src/сomponents/icon/icon.tsx
+++
@@ -1,9 +0,0 @@
-export interface IconProps {
-  src: string;
-  alt?: string;
-  className?: string;
-}
-
-export const Icon = ({ src, alt, className }: IconProps) => {
-  return <img src={src} alt={alt} className={className}/>;
-};
