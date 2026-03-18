dc3ed19e48c1f8b60fbe6901e40880a48b1411de
Artur Protsenko <aeprotsenko@avito.ru>
2026-03-18 20:11:06 +0300

    refactor: migrate shared/ui — Icon, Button, AppLink, CategoryButton, FeatureItem

---
+++ src/shared/ui/category-button/CategoryButton.tsx
@@ -0,0 +1,17 @@
+import styles from './category-button.module.css';
+
+export interface CategoryButtonProps {
+  image: { url: string; alt: string };
+  title: string;
+}
+
+export const CategoryButton = ({ image, title }: CategoryButtonProps) => {
+  return (
+    <button className={styles.categoryButton}>
+      <div className={styles.content}>
+        <img src={image.url} alt={image.alt} />
+        <h3>{title}</h3>
+      </div>
+    </button>
+  );
+};
